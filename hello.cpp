7eb8c14f6aafa9e8b60ab8c2babf9f08cee749e5
doan201203 <doan201203@gmail.com>
2022-04-23 19:06:29 +0700

    s

---
+++ hello.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+#define endl '\n'
+#define rep(i,n) for(int i = 0 ; i < (n) ; ++i)
+#define fi first
+#define se second
+#define ar array
+void setIO()
+{
+    ios_base::sync_with_stdio(0);	cin.tie(0);
+}
+
+void Solve()
+{
+    cout << "Hello World!" << endl;
+}
+
+int main()
+{
+    setIO();
+    int test=1;
+    //cin >> test;
+    while(test--){
+        Solve();
+    }
+}
